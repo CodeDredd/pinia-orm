ac89f745ac3b3e9ac9255020eebc5ad900da61ab
Gregor Becker <gregor@codedredd.de>
2022-02-09 06:09:20 +0100

    chore: add google verification for docs

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -8,6 +8,15 @@ export default theme({
   //   domainId: 'a2998bc2-56dd-47fa-9d94-9781411bd1f9',
   //   detailed: true
   // },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'google-verification',
+      name: 'google-site-verification',
+      content: '9nl6uN8TTI5jqZb_noWhW0itfRNSflXiRs99B4xFofo',
+    },
+  ],
   pwa: {
     manifest: {
       name: 'Nuxt PWA',
