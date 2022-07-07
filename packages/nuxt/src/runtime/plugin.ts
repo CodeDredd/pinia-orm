007ca29b1e26ae061b86bbe1da3b3f9804e280c8
Gregor Becker <gregor@codedredd.de>
2022-07-07 22:48:21 +0200

    refactor: fix linting

--- packages/nuxt/src/runtime/plugin.ts
+++ packages/nuxt/src/runtime/plugin.ts
@@ -1,6 +1,6 @@
-import { defineNuxtPlugin } from '#app'
 import PiniaOrm from 'pinia-orm'
 import { setActivePinia } from 'pinia'
+import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
   // eslint-disable-next-line import/no-named-as-default-member
