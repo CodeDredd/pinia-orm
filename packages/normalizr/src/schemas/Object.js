664d6ea2808e63211602bde0c1f508886dee78a0
Gregor Becker <gregor@codedredd.de>
2022-08-14 18:17:55 +0200

    build(normalizr): create own `normalizr` package (#191)
    * feat(normalizr): First release
    Reduced version of `normalizr`
    
    * chore(normalizr): update deps
    
    * chore(normalizr): update deps

---
+++ packages/normalizr/src/schemas/Object.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const normalize = (schema, input, parent, key, visit, addEntity, visitedEntities) => {
+  const object = { ...input }
+  Object.keys(schema).forEach((key) => {
+    const localSchema = schema[key]
+    const resolvedLocalSchema = typeof localSchema === 'function' ? localSchema(input) : localSchema
+    const value = visit(input[key], input, key, resolvedLocalSchema, addEntity, visitedEntities)
+    if (value === undefined || value === null) {
+      delete object[key]
+    } else {
+      object[key] = value
+    }
+  })
+  return object
+}
