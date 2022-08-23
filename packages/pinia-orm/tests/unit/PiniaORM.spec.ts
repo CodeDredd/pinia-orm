65671c8898497b43050bd9647f25aaf155e28b09
Gregor Becker <gregor@codedredd.de>
2022-08-23 13:21:14 +0200

    refactor: add hidden decorator

--- packages/pinia-orm/tests/unit/PiniaORM.spec.ts
+++ packages/pinia-orm/tests/unit/PiniaORM.spec.ts
@@ -1,22 +1,32 @@
-import { getActivePinia } from 'pinia'
 import { describe, expect, it } from 'vitest'
+import { createPinia, setActivePinia } from 'pinia'
+import { createApp } from 'vue-demi'
+import { Model, createORM, useRepo } from '../../src'
+import { Attr, Str } from '../../src/decorators'
 
 describe('unit/PiniaORM', () => {
-  it.skip('installs Vuex ORM to the store', () => {
-    // @ts-expect-error I don't know
-    const store = getActivePinia().state.value
-    expect(store.$database.started).toBe(true)
-  })
+  class User extends Model {
+    static entity = 'users'
 
-  // it.skip('can customize the namespace', () => {
-  // const store = new Vuex.Store({
-  //   plugins: [VuexORM.install({ namespace: 'database' })]
-  // })
-  //
-  // const expected = {
-  //   database: {}
-  // }
-  //
-  // expect(store.state).toEqual(expected)
-  // })
+    @Attr(0) declare id: number
+    @Str('') declare name: string
+    @Str('') declare username: string
+  }
+
+  it('pass a config so all models have meta', () => {
+    const app = createApp({})
+    const pinia = createPinia()
+    pinia.use(createORM({ model: { withMeta: true } }))
+    app.use(pinia)
+    setActivePinia(pinia)
+
+    const userRepo = useRepo(User)
+    userRepo.save({
+      id: 1,
+      name: 'John',
+      username: 'JD',
+    })
+
+    expect(userRepo.find(1)?._meta).toHaveProperty('createdAt')
+  })
 })
