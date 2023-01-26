fa65e98be9eafa9ac8dadffa3a5ebac1b1285a24
Gregor Becker <gregor@codedredd.de>
2023-01-26 20:49:51 +0100

    chore: update docus to the latest nuxt version

---
+++ docs/tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss'
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: { value: '#ecfeff' },
+          100: { value: '#cffafe' },
+          200: { value: '#a5f3fc' },
+          300: { value: '#67e8f9' },
+          400: { value: '#22d3ee' },
+          500: { value: '#06b6d4' },
+          600: { value: '#0891b2' },
+          700: { value: '#0e7490' },
+          800: { value: '#155e75' },
+          900: { value: '#164e63' }
+        }
+      }
+    }
+  }
+}
