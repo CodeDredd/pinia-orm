b441b9ac93357a293759f38efa345508ba605c14
Gregor Becker <gregor@codedredd.de>
2023-01-27 07:30:05 +0100

    chore: update docus to the latest nuxt version (#898)

--- docs/components/Logo.vue
+++ docs/components/Logo.vue
@@ -2,7 +2,7 @@
   <div class="flex">
     <img height="50" width="50" src="/logo.svg">
     <h3 class="text-2xl py-4 ml-2">
-      <span class="">Pinia </span><span class="text-primary-500">ORM</span>
+      <span class="">Pinia </span><span class="text-cyan-500">ORM</span>
     </h3>
   </div>
 </template>
