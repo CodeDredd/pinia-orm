f62fd42f2b17e1df6fc1514497fdcbfc9a05bf70
Gregor Becker <gregor@codedredd.de>
2023-03-09 20:06:49 +0100

    chore: add sponsorkit

---
+++ sponsorkit.config.js
@@ -0,0 +1,40 @@
+import { defineConfig, presets } from 'sponsorkit'
+
+export default defineConfig({
+  // Providers configs
+  github: {
+    login: 'codedredd',
+    type: 'user',
+  },
+  // Rendering configs
+  width: 800,
+  formats: ['json', 'svg', 'png'],
+  tiers: [
+    // Past sponsors, currently only supports GitHub
+    {
+      title: 'Past Sponsors',
+      monthlyDollars: -1,
+      preset: presets.xs,
+    },
+    // Default tier
+    {
+      title: 'Backers',
+      preset: presets.base,
+    },
+    {
+      title: 'Sponsors',
+      monthlyDollars: 10,
+      preset: presets.medium,
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 50,
+      preset: presets.large,
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 100,
+      preset: presets.xl,
+    },
+  ],
+})
