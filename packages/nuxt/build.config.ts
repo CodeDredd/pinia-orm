12e3ea392816b0e7bbbe790e405623467e4c6480
Gregor Becker <gregor@codedredd.de>
2023-03-28 17:36:52 +0200

    fix(nuxt): Nuxt 3 throws error on build & dev (#1136)
    closes #1135

--- packages/nuxt/build.config.ts
+++ packages/nuxt/build.config.ts
@@ -4,6 +4,5 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   rollup: {
     emitCJS: true
-  },
-  externals: ['pinia', '@nuxt/kit-edge', '@nuxt/types', 'pinia-orm', '@pinia-orm/normalizr']
+  }
 })
