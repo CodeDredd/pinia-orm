b4b0a4f31494c45aafb048a2fc9b32100ab43c1f
Gregor Becker <gregor@codedredd.de>
2024-05-03 09:12:39 +0200

    chore: Clean up playgrounds

--- playgrounds/nuxt3/nuxt.config.ts
+++ playgrounds/nuxt3/nuxt.config.ts
@@ -2,5 +2,14 @@ export default defineNuxtConfig({
   modules: [
     '@pinia/nuxt',
     '@pinia-orm/nuxt'
-  ]
+  ],
+  vite: {
+    esbuild: {
+      tsconfigRaw: {
+        compilerOptions: {
+          experimentalDecorators: true,
+        }
+      }
+    }
+  }
 })
