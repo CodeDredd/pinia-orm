ddb257e3ad87bca2213f8fe837c826993fdfa2c3
Gregor Becker <gregor@codedredd.de>
2024-06-17 21:25:13 +0200

    refactor(pinia-orm): returning type of `find` is still any

--- packages/pinia-orm/src/repository/Repository.ts
+++ packages/pinia-orm/src/repository/Repository.ts
@@ -25,6 +25,38 @@ import type { WeakCache } from '../cache/WeakCache'
 import { config as globalConfig } from '../store/Config'
 import type { FilledInstallOptions } from '../store/Store'
 
+export interface Repository<M extends Model = Model> {
+  /**
+   * Add a where clause where `field` value is in values.
+   */
+  whereIn (field: string, values: any[] | Set<any>): Query<M>
+  /**
+   * Add a where clause where `field` value is in values or ...
+   */
+  orWhereIn (field: string, values: any[] | Set<any>): Query<M>
+  /**
+   * Add a where clause where `field` value is not in values or ...
+   */
+  orWhereNotIn (field: string, values: any[] | Set<any>): Query<M>
+  /**
+   * Add a where clause where `field` has not defined values
+   */
+  whereNotIn (field: string, values: any[] | Set<any>): Query<M>
+  /**
+   * Add a where clause to get all results where `field` is null
+   */
+  whereNull (field: string): Query<M>
+  /**
+   * Add a where clause to get all results where `field` is not null
+   */
+  whereNotNull (field: string): Query<M>
+  /**
+   * Find the model with the given id.
+   */
+  find (id: string | number): Item<M>
+  find (ids: (string | number)[]): Collection<M>
+}
+
 export class Repository<M extends Model = Model> {
   [index: string]: any
   /**
