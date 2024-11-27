31cb11d770ba2193f82c416a90d349fa290fe984
Gregor Becker <gregor@codedredd.de>
2024-11-27 07:46:40 +0100

    chore(nuxt): fix nuxt ci build with "import.meta"

--- packages/nuxt/playground/nuxt.config.ts
+++ packages/nuxt/playground/nuxt.config.ts
@@ -1,11 +1,10 @@
 import { defineNuxtConfig } from 'nuxt/config'
-import PiniaOrm from '../src/module'
 
 export default defineNuxtConfig({
   modules: [
     '@nuxt/devtools',
     '@pinia/nuxt',
-    PiniaOrm,
+    '../src/module',
   ],
   // See https://github.com/nuxt/framework/issues/2371
   nitro: {
