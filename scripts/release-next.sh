7b8d6d1645a736b2e866086a71a76147f7d7f99b
Gregor Becker <gregor@codedredd.de>
2025-02-17 19:16:24 +0100

    chore: Add release next script

---
+++ scripts/release-next.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# Restore all git changes
+git restore -s@ -SW  -- packages
+
+# Build all once to ensure things are nice
+pnpm build
+
+# Release packages
+for PKG in packages/* ; do
+  pushd $PKG
+  TAG="next"
+  echo "⚡ Publishing $PKG with tag $TAG"
+  cp ../../LICENSE .
+  pnpm publish --access public --no-git-checks --tag $TAG
+  popd > /dev/null
+done
